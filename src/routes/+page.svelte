6eef348f7346f97f9cd14646717cd8e816d6cfd7
My test <dm.karpenko@istu.edu.ua>
2024-09-13 10:44:02 +0300

    add buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,5 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <strong>Hi</strong>
+
+<a class="btn btn-primary" href="/contacts">Contacts</a>
---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to Contacts page</h1>
+
+<a class="btn btn-primary" href="/">Main</a>
